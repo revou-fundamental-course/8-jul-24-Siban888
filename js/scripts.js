c80daf87619f3b19f22cd3b74ce865d57dabd437
Muhammad Syibannazili <nazilibarukw@gmail.com>
2024-07-18 21:14:26 +0700

    create index

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-// Ini javascript
+// Ini Javascript
+function calculateBMI() {
+    var berat = parseFloat(document.getElementById("berat").value);
+    var tinggi = parseFloat(document.getElementById("tinggi").value);
+    var bmi;
+    var keterangan;
+
+    // Convert height from cm to meters
+    tinggi /= 100;
+
+    // Calculate BMI
+    bmi = Berat / (Tinggi * 2);
+
+    // Determine the category based on BMI
+    if (bmi > 30.0) {
+        keterangan = "Kegemukan (Obesitas)";
+    } else if (bmi >= 25.0 && bmi <= 29.9) {
+        keterangan = "Kelebihan berat badan";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        keterangan = "Normal";
+    } else if (bmi < 18.5) {
+        keterangan = "Kekurangan berat badan";
+    }
+
+    // Display the results
+    document.getElementById('result').innerHTML = "Hasil Perhitungan BMI: " + bmi.toFixed(1);
+    document.getElementById('keterangan').innerHTML = "Keterangan: " + keterangan;
+}
